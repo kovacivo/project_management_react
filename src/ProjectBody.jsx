4aa18e7f221dabdf675ba6b02f8b0b0d22312136
kovacivo <ivan.kovac216@gmail.com>
2025-05-28 17:49:55 +0200

    Visual for form to create new project is ready.

--- src/ProjectBody.jsx
+++ src/ProjectBody.jsx
@@ -12,6 +12,10 @@ export default function ProjectBody({ project }) {
     setClickedNewProject(true);
   }
 
+  function handleSaveButton(){
+    console.log("Pressing button to save project data...");
+  }
+
   const [clickedNewProject, setClickedNewProject] = useState(false);
 
   let contentToDisplay = null;
@@ -19,7 +23,25 @@ export default function ProjectBody({ project }) {
   if (project != null) {
     contentToDisplay = <><p>project content</p><p>{project.name}</p></>;
   } else if (clickedNewProject === true) {
-    contentToDisplay = <h1>formularik</h1>;
+    contentToDisplay = <>
+      <div className="justify-self-end mr-64">
+        <button className="text-stone-800 hover:bg-stone-800 hover:text-slate-50 rounded-lg text-xl p-4">Cancel</button>
+        <button
+          className="text-stone-800 hover:bg-stone-800 hover:text-slate-50 rounded-lg text-xl p-4"
+          onClick={handleSaveButton}
+        >
+          Save
+        </button>
+      </div>
+      <div className="flex flex-col mr-64">
+        <label className="font-bold text-stone-600 pt-5">TITLE</label>
+        <input className="bg-stone-300 pt-2 pb-2 rounded-sm text-lg" type="text" placeholder="Put project name here..." />
+        <label className="font-bold text-stone-600 pt-5">DESCRIPTION</label>
+        <textarea className="bg-stone-300 pt-2 pb-2 rounded-sm text-lg" type="text" />
+        <label className="font-bold text-stone-600 pt-5">DUE DATE</label>
+        <input className="bg-stone-300 pt-2 pb-2  rounded-sm text-lg active:rounded-b-lg" type="date" />
+      </div>
+    </>;
   } else {
     contentToDisplay = <div className=" align-middle justify-items-center">
       <img src={logo} width={80} height={80} alt='logo' />
@@ -34,7 +56,7 @@ export default function ProjectBody({ project }) {
 
   return (
     <div className="w-3/4">
-      <div className="flex-row mt-32">
+      <div className="mt-32">
         {contentToDisplay}
       </div>
     </div>
