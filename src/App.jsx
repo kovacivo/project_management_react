29b6785ae478e048fc6bb51e22899650fb8c3d55
kovacivo <ivan.kovac216@gmail.com>
2025-05-28 16:06:57 +0200

    Can click to start create new project.

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,9 @@ import { useState, StrictMode } from "react";
 function App() {
 
   const projects = [
-    { 'id':1,'name': 'project #1' },
-    { 'id':2,'name': 'project #2' },
-    { 'id':3, 'name': 'project #3' },
+    // { 'id':1,'name': 'project #1' },
+    // { 'id':2,'name': 'project #2' },
+    // { 'id':3, 'name': 'project #3' },
   ]
   //const [projects, setProjects] = useState();
 
@@ -16,7 +16,7 @@ function App() {
       <div className="h-11 bg-slate-50"></div>
       <div className="flex gap-12">
         <ProjectMenu projects={projects} />
-        <ProjectBody projects={projects} />
+        <ProjectBody project={projects[0]} />
       </div>
     </StrictMode>
   );
--- src/ProjectBody.jsx
+++ src/ProjectBody.jsx
@@ -1,20 +1,41 @@
 import logo from './assets/no-projects.png';
+import { useState } from 'react';
 
-function handleCreateProject(){
-  console.log("Pressing button to create new project...");
-}
 
-export default function ProjectBody({ projects }) {
+
+
+
+export default function ProjectBody({ project }) {
+
+  function handleCreateProject() {
+    console.log("Pressing button to create new project...");
+    setClickedNewProject(true);
+  }
+
+  const [clickedNewProject, setClickedNewProject] = useState(false);
+
+  let contentToDisplay = null;
+
+  if (project != null) {
+    contentToDisplay = <><p>project content</p><p>{project.name}</p></>;
+  } else if (clickedNewProject === true) {
+    contentToDisplay = <h1>formularik</h1>;
+  } else {
+    contentToDisplay = <div className=" align-middle justify-items-center">
+      <img src={logo} width={80} height={80} alt='logo' />
+      <h3 className="text-stone-800 font-bold text-2xl mt-5 mb-5">No Project Selected</h3>
+      <h3 className="text-stone-400 font-medium text-xl mb-5">Select a project or get started with a new one</h3>
+      <button
+        className="bg-stone-800 rounded-lg text-stone-400 text-xl p-3 mt-3"
+        onClick={handleCreateProject}>Create new project</button>
+
+    </div>;
+  }
+
   return (
     <div className="w-3/4">
       <div className="flex-row mt-32">
-        <div className=" align-middle justify-items-center">
-          <img src={logo} width={80} height={80} alt='logo' />
-          <h3 className="text-stone-800 font-bold text-2xl mt-5 mb-5">No Project Selected</h3>
-          <h3 className="text-stone-400 font-medium text-xl mb-5">Select a project or get started with a new one</h3>
-          <button className="bg-stone-800 rounded-lg text-stone-400 text-xl p-3 mt-3" onClick={handleCreateProject}>Create new project</button>
-
-        </div>
+        {contentToDisplay}
       </div>
     </div>
   );
